518731091335aab72b23779e9c1151bbd6863d37
RubenFontes <rubencfontes17@gmail.com>
2022-07-28 17:19:53 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.listen('3000');
+
+//middleware
+app.use(express.json());
+
+let author = "Ruben Fontes";
+
+app.route('/').get((req, res) => {
+    res.send(author);
+});
+
+app.route('/').post((req, res) => {
+    res.send(req.body);
+});
+
+app.route('/').put((req, res) => {
+    author = req.body.author;
+    res.send(author);
+});
+
+app.route('/:id').delete((req, res) => {
+    author = ""; 
+    res.send(req.params.id);
+});
